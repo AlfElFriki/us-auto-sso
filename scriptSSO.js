67bb6c0a4d9aab718a442cffe45b42829ce0aa92
Alfonso <115218712+AlfEspadero@users.noreply.github.com>
2026-02-10 15:02:36 +0100

    Update copyright notice in scriptSSO.js

--- scriptSSO.js
+++ scriptSSO.js
@@ -14,7 +14,7 @@
 
 /* MIT License
  *
- * Copyright (c) 2025 Alf
+ * Copyright (c) 2025 Alfonso Espadero
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
